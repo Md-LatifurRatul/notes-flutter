7b050998f013aabc05537e7c73148d3d60381f82
Md-LatifurRatul <latifurratulislam@gmail.com>
2025-05-15 21:26:20 +0600

    feat: sign-out integration, fix: controller-binder, navigation

--- lib/controller_binder.dart
+++ lib/controller_binder.dart
@@ -1,14 +1,15 @@
 import 'package:get/get.dart';
 import 'package:notes_flutter/controllers/obsecure_text_controller.dart';
 import 'package:notes_flutter/controllers/sign_in_controller.dart';
+import 'package:notes_flutter/controllers/sign_out_controller.dart';
 import 'package:notes_flutter/controllers/sign_up_controller.dart';
 
 class ControllerBinder extends Bindings {
   @override
   void dependencies() {
-    Get.lazyPut(() => ObsecureTextController());
-    Get.lazyPut(() => SignInController());
-    Get.lazyPut(() => SignUpController());
-    Get.lazyPut(() => SignUpController());
+    Get.put(ObsecureTextController());
+    Get.put(SignInController());
+    Get.put(SignUpController());
+    Get.put(SignOutController());
   }
 }
--- lib/presentation/pages/home/notes_flutter_home_screen.dart
+++ lib/presentation/pages/home/notes_flutter_home_screen.dart
@@ -1,10 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:go_router/go_router.dart';
+import 'package:notes_flutter/controllers/sign_out_controller.dart';
+import 'package:notes_flutter/routes/app_routes.dart';
+import 'package:notes_flutter/widgets/confirm_dialog.dart';
+import 'package:notes_flutter/widgets/snack_message.dart';
 
 class NotesFlutterHomeScreen extends StatelessWidget {
   const NotesFlutterHomeScreen({super.key});
 
+  Future<void> _signOutUser(BuildContext context) async {
+    final SignOutController signOutController = Get.find<SignOutController>();
+
+    final success = await signOutController.signOut();
+    if (success) {
+      SnackMessage.shownackMessage(
+        context: context,
+        message: "Sign out successful",
+        type: SnackType.success,
+      );
+
+      context.pushReplacement(AppRoutes.login);
+    } else {
+      SnackMessage.shownackMessage(
+        context: context,
+        message: signOutController.errorMessage,
+        type: SnackType.error,
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Notes"),
+        centerTitle: true,
+        backgroundColor: Colors.amber,
+        actions: [
+          IconButton(
+            style: IconButton.styleFrom(backgroundColor: Colors.yellow),
+            onPressed: () async {
+              ConfirmDialog.showAlertDialogue(
+                context,
+                title: "Sign Out",
+                content: "Are you sure you want to log-out?",
+                confirmString: "Log-out",
+                onPressed: () {
+                  _signOutUser(context);
+                },
+              );
+            },
+            icon: Icon(Icons.logout),
+          ),
+        ],
+      ),
+    );
   }
 }
